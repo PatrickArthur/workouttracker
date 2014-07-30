d83443ce136c5c1155cda21fd9369174fd81347c
Patrick <pa@arthurfamily.ws>
2014-07-30 19:04:17 -0400

    Devise change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,38 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+   # layout :layout_by_resource
+
+
+  def after_sign_in_path_for(user)
+    user.profile.present? ? profiles_path : new_profile_path
+  end
+
+  layout Proc.new { |controller| controller.devise_controller? ? 'devise' : 'application' }
+
+  # def set_new_user
+  #   @new_user = User.new
+  # end
+
+  # protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :set_new_user
+
+  protected
+
+   def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730220000) do
+ActiveRecord::Schema.define(version: 20140730230250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140730220000) do
     t.integer "calories"
   end
 
-  create_table "models", force: true do |t|
+  create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20140730220000) do
     t.datetime "updated_at"
   end
 
-  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "workouts", force: true do |t|
     t.integer "user_id"
