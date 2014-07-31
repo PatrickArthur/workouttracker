1c63767f31dd10f0f182b811ed5627cb1a470a4f
Patrick <pa@arthurfamily.ws>
2014-07-30 20:26:34 -0400

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730230956) do
+ActiveRecord::Schema.define(version: 20140731001520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140730230956) do
     t.integer "neck"
     t.integer "integer"
     t.string  "level"
+    t.string  "gender"
   end
 
   create_table "users", force: true do |t|
